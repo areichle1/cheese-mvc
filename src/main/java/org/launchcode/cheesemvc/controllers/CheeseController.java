1a039f3df4828202a96870ab7eddcad5ba00a587
Andrea Reichle <andrea_reichle@yahoo.com>
2019-08-10 14:32:12 -0500

    Added thymeleaf, added index.html file, updated to request path to be /cheese, added bootrun so don't have to stop and restart server every time make changes to templates, finished tutorial Intro to Spring Boot: Views (Part 1)

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -2,17 +2,18 @@ package org.launchcode.cheesemvc.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("cheese")
 public class CheeseController {
 
-
+    //Request path: /cheese
     @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "My Cheese";
+    public String index(Model model) {
+        model.addAttribute("title", "My Cheeses" );
+        return "cheese/index";
     }
 
 }
