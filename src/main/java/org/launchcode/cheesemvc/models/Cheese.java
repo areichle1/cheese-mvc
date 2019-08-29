12dfc336f06f611387be056f747eb6eb4aeeccec
Andrea Reichle <andrea_reichle@yahoo.com>
2019-08-29 15:31:48 -0500

    class 9 lessons walk through part 2 completed, added enum CheeseType

--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -13,6 +13,8 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
+    private CheeseType type;
+
     private int cheeseId;
     private static int nextId = 1;
 
@@ -49,5 +51,13 @@ public class Cheese {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public CheeseType getType() {
+        return type;
+    }
+
+    public void setType(CheeseType type) {
+        this.type = type;
     }
 }
