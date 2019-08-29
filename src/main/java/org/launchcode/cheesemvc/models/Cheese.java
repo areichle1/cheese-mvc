fe2d547fffa3c8b1263f255789c6552f0f477c45
Andrea Reichle <andrea_reichle@yahoo.com>
2019-08-29 10:11:32 -0500

    class 9 lessons walk through part 1 completed, annotated the models using Java Validation API annotations, checks that bound model objects are valid, displays error messages in the view

--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -1,9 +1,18 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Cheese {
 
+    @NotNull
+    @Size(min=3, max=15)
     private String name;
+
+    @NotNull
+    @Size(min=1, message = "Description must not be empty")
     private String description;
+
     private int cheeseId;
     private static int nextId = 1;
 
